9ad7df7cbd232343a266db0f6e95f874c76caab8
Mateusz Janda <mateusz.janda@gmail.com>
2024-02-13 18:10:56 +0100

    Hello world on display

--- thermometer.ino
+++ thermometer.ino
@@ -1,24 +1,58 @@
-#include <DallasTemperature.h>
 #include <OneWire.h>
+#include <DallasTemperature.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 #define ONE_WIRE_PIN 2
 
 OneWire oneWire(ONE_WIRE_PIN);
-DallasTemperature sensors(&oneWire);
+DallasTemperature temp_sensor(&oneWire);
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for SSD1306 display connected using software SPI (default case):
+#define OLED_MOSI  11
+#define OLED_CLK   13
+#define OLED_CS    10
+#define OLED_DC     7
+#define OLED_RESET  8
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
+  OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
 void setup()
 {
     // Start UART communication
     Serial.begin(9600);
+
     // Start DS18B20 sensor
-    sensors.begin();
-    Serial.println("Setup done.");
+    temp_sensor.begin();
+    Serial.println("Temperature sensor setup done.");
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    // Don't proceed after thath
+    for(;;); 
+  }
+
+  display.setRotation(90);
+    // Clear the buffer
+  display.clearDisplay();
+
+    display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.println(F("Hello, world!"));
+  display.display();
 }
 
 void loop()
 {
-    sensors.requestTemperatures();
+    temp_sensor.requestTemperatures();
     Serial.print("Temp: ");
-    Serial.println(sensors.getTempCByIndex(0));
+    Serial.println(temp_sensor.getTempCByIndex(0));
     delay(1000);
 }
