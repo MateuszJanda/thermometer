9df233558dfcea1e77774a3b36bdb1f59c9f0eda
Mateusz Janda <mateusz.janda@gmail.com>
2024-02-13 19:19:08 +0100

    Refactor

--- thermometer.ino
+++ thermometer.ino
@@ -1,27 +1,30 @@
-#include <OneWire.h>
-#include <DallasTemperature.h>
-#include <SPI.h>
-#include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <DallasTemperature.h>
+#include <OneWire.h>
+#include <SPI.h>
+#include <Wire.h>
 
 #define ONE_WIRE_PIN 2
 #define DEVICE_INDEX 0
 
-OneWire oneWire(ONE_WIRE_PIN);
-DallasTemperature temp_sensor(&oneWire);
+OneWire one_wire(ONE_WIRE_PIN);
+DallasTemperature temp_sensor(&one_wire);
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+// OLED display width, in pixels
+#define OLED_SCREEN_WIDTH 128
+// OLED display height, in pixels
+#define OLED_SCREEN_HEIGHT 64
 
-// Declaration for SSD1306 display connected using software SPI (default case):
-#define OLED_MOSI  11
-#define OLED_CLK   13
-#define OLED_CS    10
-#define OLED_DC     7
-#define OLED_RESET  8
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
-  OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+// Declaration for SSD1306 display connected using software 4-wire SPI (default case):
+#define OLED_MOSI_PIN 11
+#define OLED_SCLK_PIN 13
+#define OLED_CS_PIN 10
+#define OLED_DC_PIN 7
+#define OLED_RST_PIN 8
+
+Adafruit_SSD1306 display(
+    OLED_SCREEN_WIDTH, OLED_SCREEN_HEIGHT, OLED_MOSI_PIN, OLED_SCLK_PIN, OLED_DC_PIN, OLED_RST_PIN, OLED_CS_PIN);
 
 void setup()
 {
@@ -32,23 +35,20 @@ void setup()
     temp_sensor.begin();
     Serial.println("Temperature sensor setup done.");
 
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    // Don't proceed after thath
-    for(;;); 
-  }
+    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+    if (!display.begin(SSD1306_SWITCHCAPVCC)) {
+        Serial.println(F("SSD1306 allocation failed"));
+        // Don't proceed after that
+        for (;;) { }
+    }
 
-  display.setRotation(90);
-
+    // Rotate display by 180
+    display.setRotation(2);
     // Clear the buffer
-  display.clearDisplay();
-
-    // display.setTextSize(1);             // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE);        // Draw white text
-  // display.setCursor(0,0);             // Start at top-left corner
-  // display.println(F("Hello, world!"));
-  display.display();
+    display.clearDisplay();
+    // Set white font color
+    display.setTextColor(SSD1306_WHITE);
+    display.display();
 }
 
 void loop()
@@ -58,48 +58,29 @@ void loop()
     float temperature = temp_sensor.getTempCByIndex(DEVICE_INDEX);
     Serial.println("Temp: " + String(temperature));
 
-    // Print temperature on display
+    // Display temperature value in Celsius
     display.clearDisplay();
+    display.setTextSize(3);
+    display.setCursor(0, 0);
+    display.println(String(temperature) + "C");
 
-    
-        display.setTextSize(3);             // Normal 1:1 pixel scale
-  // display.setTextColor(SSD1306_WHITE);        // Draw white text
-  display.setCursor(0,0);             // Start at top-left corner
-  // display.println(F(String(temperature).c_str()));
-  // display.println("aaa");
-  // display.println(F("aaa"));
-  // temperature *= -10;
-  display.println(String(temperature) + "C"  );
-  
-    
-  // Print temperature sensor id
-  DeviceAddress device_address = {};
+    // Print unique 64bit temperature sensor id (on 1-Wire)
+    DeviceAddress device_address = {};
     bool result = temp_sensor.getAddress(device_address, DEVICE_INDEX);
-    if (result)
-    {
-      Serial.println("Device address OK");
-      display.setTextSize(1);  
-      display.setCursor(0,48);
-// display.println(String(address));
+    if (result) {
+        Serial.println("Device address OK");
+        display.setTextSize(1);
+        display.setCursor(0, 48);
 
-  Serial.println();
-char strBuf[50] = {};
-    for (size_t i = 0; i < 8; i++)
-    {
-      Serial.println(device_address[i], HEX);
+        char strBuf[50] = {};
+        for (size_t i = 0; i < 8; i++) {
+            Serial.println(device_address[i], HEX);
             sprintf(&strBuf[i * 2], "%02X", device_address[i]);
-      // sprintf(strBuf + (i * 2), "%02X", 9);
-
-      
-
+        }
+        display.print("Dev" + String(DEVICE_INDEX) + ":");
+        display.println(strBuf);
     }
-    Serial.println();
-    display.print("Dev" + String(DEVICE_INDEX) + ":");
-    display.println(strBuf);
-    // display.println("Here");
 
-  }
-
-display.display();
+    display.display();
     delay(500);
 }
