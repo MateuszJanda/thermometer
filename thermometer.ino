b9f01480efcc1165a43a6648e89d5aaca1dcf558
Mateusz Janda <mateusz.janda@gmail.com>
2024-02-13 19:34:47 +0100

    Device address read in setup

--- thermometer.ino
+++ thermometer.ino
@@ -11,9 +11,8 @@
 OneWire one_wire(ONE_WIRE_PIN);
 DallasTemperature temp_sensor(&one_wire);
 
-// OLED display width, in pixels
+// OLED display resolution in pixels
 #define OLED_SCREEN_WIDTH 128
-// OLED display height, in pixels
 #define OLED_SCREEN_HEIGHT 64
 
 // Declaration for SSD1306 display connected using software 4-wire SPI (default case):
@@ -26,25 +25,40 @@ DallasTemperature temp_sensor(&one_wire);
 Adafruit_SSD1306 display(
     OLED_SCREEN_WIDTH, OLED_SCREEN_HEIGHT, OLED_MOSI_PIN, OLED_SCLK_PIN, OLED_DC_PIN, OLED_RST_PIN, OLED_CS_PIN);
 
+// Unique (64-bit) temperature sensor ID (on 1-Wire)
+DeviceAddress device_address = {};
+char device_address_str[20] = {};
+
 void setup()
 {
-    // Start UART communication
+    // Start UART baud (communication speed)
     Serial.begin(9600);
 
     // Start DS18B20 sensor
     temp_sensor.begin();
     Serial.println("Temperature sensor setup done.");
 
+    // Read unique (64-bit) temperature sensor ID (on 1-Wire)
+    bool result = temp_sensor.getAddress(device_address, DEVICE_INDEX);
+    if (result) {
+        device_address_to_string();
+        Serial.print("Temperature sensor ID: ");
+        Serial.println(device_address_str);
+    } else {
+        Serial.println("Can't read temperature sensor ID on 1-Wire.");
+    }
+
+    // Start SSD1306 display.
     // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
     if (!display.begin(SSD1306_SWITCHCAPVCC)) {
-        Serial.println(F("SSD1306 allocation failed"));
+        Serial.println(F("SSD1306 allocation failed."));
         // Don't proceed after that
         for (;;) { }
     }
 
     // Rotate display by 180
     display.setRotation(2);
-    // Clear the buffer
+    // Clear display screen
     display.clearDisplay();
     // Set white font color
     display.setTextColor(SSD1306_WHITE);
@@ -56,7 +70,7 @@ void loop()
     // Read temperature from sensor
     temp_sensor.requestTemperatures();
     float temperature = temp_sensor.getTempCByIndex(DEVICE_INDEX);
-    Serial.println("Temp: " + String(temperature));
+    Serial.println("Temp: " + String(temperature) + "C");
 
     // Display temperature value in Celsius
     display.clearDisplay();
@@ -64,23 +78,19 @@ void loop()
     display.setCursor(0, 0);
     display.println(String(temperature) + "C");
 
-    // Print unique 64bit temperature sensor id (on 1-Wire)
-    DeviceAddress device_address = {};
-    bool result = temp_sensor.getAddress(device_address, DEVICE_INDEX);
-    if (result) {
-        Serial.println("Device address OK");
-        display.setTextSize(1);
-        display.setCursor(0, 48);
-
-        char strBuf[50] = {};
-        for (size_t i = 0; i < 8; i++) {
-            Serial.println(device_address[i], HEX);
-            sprintf(&strBuf[i * 2], "%02X", device_address[i]);
-        }
-        display.print("Dev" + String(DEVICE_INDEX) + ":");
-        display.println(strBuf);
-    }
+    // Print unique (64-bit) temperature sensor ID (on 1-Wire)
+    display.setTextSize(1);
+    display.setCursor(0, 48);
+    display.print("Dev" + String(DEVICE_INDEX) + ":");
+    display.println(device_address_str);
 
     display.display();
     delay(500);
+}
+
+void device_address_to_string()
+{
+    for (size_t i = 0; i < sizeof(device_address); i++) {
+        sprintf(&device_address_str[i * 2], "%02X", device_address[i]);
+    }
 }
